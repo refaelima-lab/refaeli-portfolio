908caf324b163509b6f0b54b84f2fc0f97d51f40
Refaeli Ma <refaelima.work@gmail.com>
2026-06-17 17:35:36 -0700

    home and welcome screen update

---
+++ assets/star/app/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/QU2YgGZsNJzys96A/scene.splinecode" 
+      />
+    </main>
+  );
+}
